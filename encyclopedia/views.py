95ca8ddec1d4eb5170539fad7ac7072e99006615
Margarita Sophia <margarita.sophia@gmail.com>
2022-01-31 20:10:19 +0200

    update

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from markdown2 import Markdown
+from decouple import config
+from random import choice 
 
 from . import util
 
@@ -8,14 +11,24 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
 def title(request, title):
     wiki_page = util.get_entry(title)
 
     if (not wiki_page):
-        return render(request, "encyclopedia/fail.html", {
+        return render(request, "encyclopedia/nonExistingEntry.html", {
           "message": "Not Found",
           "status": 404
         })
+
+
+    html_wiki_page = Markdown().convert(wiki_page)
+
+    return render(request, "encyclopedia/title.html", {
+        "title": title,
+        "html_page": html_wiki_page
+    })
+
 
 def search(request):
     search_key = request.POST.get("q", "")
@@ -30,4 +43,110 @@ def search(request):
 
     return render(request, "encyclopedia/index.html", {
         "entries": filtered_entries
-    })        
+    })
+
+def new(request):
+    if request.method == 'GET':
+        return render(request, "encyclopedia/editor.html")
+    
+    if request.method == 'POST':
+        if not "is-admin" in request.session:
+            return render(request, "encyclopedia/nonExistingEntry.html", {
+                "message": "Forbidden",
+                "description": "You have to connect as an admin",
+                "status": 403
+            }) 
+
+        file_content = request.POST.get("content","")
+        file_title = request.POST.get("title", "")
+
+        if (len(file_content) == 0 or len(file_title) == 0):
+            return render(request, "encyclopedia/nonExistingEntry.html", {
+                "message": "Bad Request",
+                "description": "Title and content cannot be empty",
+                "status": 400
+            }) 
+
+        if util.get_entry(file_title):
+            return render(request, "encyclopedia/nonExistingEntry.html", {
+                "message": "Bad Request",
+                "description": "This page already exists",
+                "status": 400
+            }) 
+
+        util.save_entry(file_title, file_content)
+
+        return redirect('title', title=file_title)
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'encyclopedia/login.html')
+
+    if request.method == 'POST':
+        password = request.POST.get("password", "")
+        
+        is_admin = password == config("ADMIN_PASSWORD")
+        
+        if not is_admin:
+            return render(request, "encyclopedia/fail.html", {
+                "message": "Unauthorized",
+                "description": "Wrong password",
+                "status": 401
+            }) 
+
+    request.session["is-admin"] = True
+
+    return redirect('index')
+
+def logoff(request):
+    del request.session["is-admin"]
+    return redirect('index')    
+
+
+def edit(request, title):
+    if request.method == 'GET':
+
+        wiki_page = util.get_entry(title)
+
+        if (not wiki_page):
+            return render(request, "encyclopedia/fail.html", {
+              "message": "Not Found",
+              "status": 404
+            })
+
+        return render(request, "encyclopedia/editor.html", {
+            "title": title,
+            "md_page": wiki_page
+        })
+
+    if request.method == 'POST':
+        if not "is-admin" in request.session:
+            return render(request, "encyclopedia/fail.html", {
+                "message": "Forbidden",
+                "description": "You have to connect as an admin",
+                "status": 403
+            }) 
+
+        file_content = request.POST.get("content","")
+        file_title = request.POST.get("title", "")
+
+        print(file_content)
+        print(file_title)
+
+        if (len(file_content) == 0 or len(file_title) == 0):
+            return render(request, "encyclopedia/fail.html", {
+                "message": "Bad Request",
+                "description": "Title and content cannot be empty",
+                "status": 400
+            }) 
+
+        util.save_entry(file_title, file_content)
+
+        return redirect('title', title=file_title)    
+
+def random(request):
+    entries = util.list_entries()
+    selected_choice = choice(entries)
+    
+    return redirect('title', title=selected_choice)
+    
