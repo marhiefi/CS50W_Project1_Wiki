e8fb74848a6524afc9759c875a8e6988660ee5b1
Margarita Sophia <margarita.sophia@gmail.com>
2022-01-31 18:06:01 +0200

    update

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    #path("wiki/<str:entry>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.title, name="title"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,26 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    wiki_page = util.get_entry(title)
+
+    if (not wiki_page):
+        return render(request, "encyclopedia/fail.html", {
+          "message": "Not Found",
+          "status": 404
+        })
+
+def search(request):
+    search_key = request.POST.get("q", "")
+    
+    entries = util.list_entries()
+    exists = search_key in entries
+    
+    if exists:
+        return redirect('title', title=search_key)
+
+    filtered_entries = [entry for entry in entries if not entry.find(search_key) == -1]
+
+    return render(request, "encyclopedia/index.html", {
+        "entries": filtered_entries
+    })        
